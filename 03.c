e6bde4595b7dce4442a1085a01b3f659b3d35df5
xubzero <allannjugush@gmail.com>
2021-12-14 17:18:15 +0300

    New update

---
+++ 03.c
@@ -0,0 +1,20 @@
+/*
+Edabit : Challenge one
+Rank : Very Easy
+Language : C
+Challenge : challengedescription here
+
+*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+void somefunction(void);
+int main(int argc,char **argv){
+	printf("This is the first function\n");
+
+	return 0;
+}
+
+void somefunction(void);
